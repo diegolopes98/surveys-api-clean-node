cb564bc8d2e4c5c1fdccc406cae4ef0b28a7fa16
Diego Pereira Lopes <diegoplopes5@gmail.com>
2020-11-28 17:37:36 -0300

    test: add signup controller test

---
+++ src/presentation/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any@mail.com',
+        password: 'any_pass',
+        password_confirmation: 'any_pass'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toEqual(400)
+  })
+})
