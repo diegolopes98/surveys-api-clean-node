b6ff5863b0845d827fe42a880a88b5a85cf7e397
Diego Pereira Lopes <diegoplopes5@gmail.com>
2020-12-07 00:18:32 -0300

    refactor: move makeSut to out of describe

--- src/infra/cripto/bcrypt-adapter.spec.ts
+++ src/infra/cripto/bcrypt-adapter.spec.ts
@@ -8,20 +8,20 @@ jest.mock('bcrypt', () => ({
   }
 }))
 
-describe('Bcrypt Adapter', () => {
-  interface SutTypes {
-    sut: Encrypter
-    salt: number
+interface SutTypes {
+  sut: Encrypter
+  salt: number
+}
+const makeSut = (): SutTypes => {
+  const salt = 12
+  const sut = new BcryptAdapter(salt)
+  return {
+    sut,
+    salt
   }
-  const makeSut = (): SutTypes => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
-    return {
-      sut,
-      salt
-    }
-  }
+}
 
+describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct values', async () => {
     const { sut, salt } = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
