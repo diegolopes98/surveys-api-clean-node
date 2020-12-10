b68f98654a6709af1a4d8852a0d3e9f7d2396228
Diego Pereira Lopes <diegoplopes5@gmail.com>
2020-12-10 15:07:06 -0300

    feat: ensure express signup route is working

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import setMiddlewares from './middlewares'
+import setRoutes from './routes'
 
 const app = express()
 
 setMiddlewares(app)
+
+setRoutes(app)
 
 export default app
