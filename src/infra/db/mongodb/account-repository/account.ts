a83b1e68e4de5e51e6479e9b6fca919b37e05dfa
Diego Pereira Lopes <diegoplopes5@gmail.com>
2020-12-11 19:23:34 -0300

    refactor: create index.ts to mongo helper folder

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,7 +1,7 @@
 import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
-import { MongoHelper } from '../helpers/mongo/mongo-helper'
+import { MongoHelper } from '../helpers/mongo'
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
