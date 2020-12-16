e022bfd8149d1cd47debc60de3a63b0a1d0b81f4
Diego Pereira Lopes <diegoplopes5@gmail.com>
2020-12-16 01:28:39 -0300

    test: ensure LogMongoRepository creates an error doc

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,40 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo'
+import { LogMongoRepository } from './log'
+
+interface SutTypes {
+  sut: LogMongoRepository
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new LogMongoRepository()
+  return {
+    sut
+  }
+}
+
+describe('Mongo Repository: Log', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  describe('Log: Error', () => {
+    let errorsCollection: Collection
+
+    beforeEach(async () => {
+      errorsCollection = await MongoHelper.getCollection('errors')
+      await errorsCollection.deleteMany({})
+    })
+
+    test('Should create an error document on success ', async () => {
+      const { sut } = makeSut()
+      await sut.logError('any_stack')
+      const count = await errorsCollection.countDocuments()
+      expect(count).toBe(1)
+    })
+  })
+})
