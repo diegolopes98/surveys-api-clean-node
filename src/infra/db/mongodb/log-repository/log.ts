3a61ca38717d6f30fcebea1368866f7bd25ac900
Diego Pereira Lopes <diegoplopes5@gmail.com>
2020-12-16 01:28:15 -0300

    feat: ensure LogMongoRepository creates an error doc

---
+++ src/infra/db/mongodb/log-repository/log.ts
@@ -0,0 +1,13 @@
+import { Collection } from 'mongodb'
+import { LogErrorRepository } from '../../../../data/protocols/log-error-repository'
+import { MongoHelper } from '../helpers/mongo'
+
+export class LogMongoRepository implements LogErrorRepository {
+  async logError (stack: string): Promise<void> {
+    const errorCollection: Collection = await MongoHelper.getCollection('errors')
+    await errorCollection.insertOne({
+      stack,
+      date: new Date()
+    })
+  }
+}
