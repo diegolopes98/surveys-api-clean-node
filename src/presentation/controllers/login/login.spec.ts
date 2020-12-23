81b2987774844dc7ff2b37e6cb37e9c111c36d80
Diego Pereira Lopes <diegoplopes5@gmail.com>
2020-12-23 01:45:52 -0300

    test: ensure LoginController validate if email is missing

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,35 @@
+import { MissingParamError } from '../../errors'
+import { badRequest } from '../../helpers/http-helper'
+import { HttpRequest, HttpResponse } from './login-protocols'
+import { LoginController } from './login'
+
+interface SutTypes {
+  sut: LoginController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new LoginController()
+  return {
+    sut
+  }
+}
+
+const makeFakeData = (): any => ({
+  email: 'any_mail@mail.com',
+  password: 'any_password'
+})
+
+const makeFakeRequest = (data): HttpRequest => ({
+  body: {
+    ...data
+  }
+})
+
+describe('Controller: Login', () => {
+  test('Should return 400 if missing email', async () => {
+    const { sut } = makeSut()
+    const { email, ...data } = makeFakeData()
+    const response: HttpResponse = await sut.handle(makeFakeRequest(data))
+    expect(response).toEqual(badRequest(new MissingParamError('email')))
+  })
+})
