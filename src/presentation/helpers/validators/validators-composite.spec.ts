a416f3e27124970e406de880b443a42f97b42018
Diego Pereira Lopes <diegoplopes5@gmail.com>
2020-12-26 04:10:59 -0300

    feat: ensure ValidatorsComposite returns error if one validator return an error

---
+++ src/presentation/helpers/validators/validators-composite.spec.ts
@@ -0,0 +1,41 @@
+import { Validator } from '../../protocols'
+import { ValidatorComposite } from './validators-composite'
+
+interface SutTypes {
+  sut: ValidatorComposite
+  validatorsStub: Validator[]
+}
+
+const makeValidatorStub = (): Validator => {
+  class ValidatorStub implements Validator {
+    validate (input: any): Error {
+      return undefined
+    }
+  }
+  return new ValidatorStub()
+}
+
+const makeValidatorsStub = (): Validator[] => {
+  const validatorsStub: Validator[] = [
+    makeValidatorStub()
+  ]
+  return validatorsStub
+}
+
+const makeSut = (): SutTypes => {
+  const validatorsStub = makeValidatorsStub()
+  const sut = new ValidatorComposite(validatorsStub)
+  return {
+    sut,
+    validatorsStub
+  }
+}
+
+describe('Validators: Composite', () => {
+  test('Should return error if one validator return an error', () => {
+    const { sut, validatorsStub } = makeSut()
+    jest.spyOn(validatorsStub[0], 'validate').mockReturnValueOnce(new Error('test_error'))
+    const error = sut.validate({ test: 'test' })
+    expect(error).toEqual(new Error('test_error'))
+  })
+})
