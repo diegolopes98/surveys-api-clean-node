5a163d06ae89992a7c97a9e34146e12b8676ad44
Diego Pereira Lopes <diegoplopes5@gmail.com>
2020-12-26 03:13:47 -0300

    feat: ensure SignupController calls validator with correct values

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,19 +1,21 @@
-import { Controller, EmailValidator, HttpRequest, HttpResponse, AddAccount } from './signup-protocols'
+import { Controller, EmailValidator, HttpRequest, HttpResponse, AddAccount, Validator } from './signup-protocols'
 import { MissingParamError, InvalidParamError } from '../../errors'
-import { badRequest, serverError, ok } from '../../helpers'
-import { checkMissingParams } from '../../helpers/controller-helper'
+import { badRequest, serverError, ok, checkMissingParams } from '../../helpers'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
   private readonly addAccount: AddAccount
+  private readonly validator: Validator
 
-  constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
+  constructor (emailValidator: EmailValidator, addAccount: AddAccount, validator: Validator) {
     this.emailValidator = emailValidator
     this.addAccount = addAccount
+    this.validator = validator
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validator.validate(httpRequest.body)
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       const missingParam = checkMissingParams(requiredFields, httpRequest)
       if (missingParam) return badRequest(new MissingParamError(missingParam))
