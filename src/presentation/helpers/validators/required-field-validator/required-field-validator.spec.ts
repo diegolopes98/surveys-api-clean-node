294d88e13c8e0ae37bd744da972107d9ee38fb19
Diego Pereira Lopes <diegoplopes5@gmail.com>
2020-12-26 03:55:39 -0300

    test: ensure required-field-validator returns an error if field is not provided

---
+++ src/presentation/helpers/validators/required-field-validator/required-field-validator.spec.ts
@@ -0,0 +1,19 @@
+import { MissingParamError } from '../../../errors'
+import { RequiredFieldValidator } from './required-field-validator'
+
+interface SutTypes {
+  sut: RequiredFieldValidator
+}
+const makeSut = (): SutTypes => {
+  const sut = new RequiredFieldValidator('test')
+  return {
+    sut
+  }
+}
+describe('Validator: Required Field', () => {
+  test('Should return MissingParamError when field is not provided', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({})
+    expect(error).toEqual(new MissingParamError('test'))
+  })
+})
