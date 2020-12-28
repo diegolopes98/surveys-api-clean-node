a9c2cb60d0a66fd01fe0476dd64fbf014befe358
Diego Pereira Lopes <diegoplopes5@gmail.com>
2020-12-28 01:59:44 -0300

    test: ensure EmailValidator return InvalidParamError when invalid email is provided

---
+++ src/presentation/helpers/validators/email-validator/email-validator.spec.ts
@@ -0,0 +1,41 @@
+import { InvalidParamError } from '../../../errors'
+import { EmailValidator } from './email-validator'
+import { EmailValidator as EmailValidatorAdapter } from '../../../protocols/email-validator'
+
+const makeEmailValidatorStub = (): EmailValidatorAdapter => {
+  class EmailValidatorStub implements EmailValidatorAdapter {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+
+interface SutTypes {
+  sut: EmailValidator
+  emailValidatorStub: EmailValidatorAdapter
+}
+
+const makeSut = (): SutTypes => {
+  const emailValidatorStub = makeEmailValidatorStub()
+  const sut = new EmailValidator('email', emailValidatorStub)
+  return {
+    sut,
+    emailValidatorStub
+  }
+}
+
+describe('Validator: Email', () => {
+  test('Should return InvalidParamError when invalid email is provided', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const error = sut.validate({})
+    expect(error).toEqual(new InvalidParamError('email'))
+  })
+
+  test('Should not return InvalidParamError when a valid email is provided', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({})
+    expect(error).toBeUndefined()
+  })
+})
