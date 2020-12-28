656eb0db03d3593b8f2709f154c63b6cd318fa68
Diego Pereira Lopes <diegoplopes5@gmail.com>
2020-12-28 01:38:17 -0300

    test: ensure CompareFieldValidator returns an Error when fields does not match

---
+++ src/presentation/helpers/validators/compare-field-validator/compare-field-validator.spec.ts
@@ -0,0 +1,20 @@
+import { InvalidParamError } from '../../../errors'
+import { CompareFieldValidator } from './compare-field-validator'
+
+interface SutTypes {
+  sut: CompareFieldValidator
+}
+const makeSut = (param1: string, param2: string): SutTypes => {
+  const sut = new CompareFieldValidator(param1, param2)
+  return {
+    sut
+  }
+}
+
+describe('Validator: Compare Field', () => {
+  test('Should return InvalidParamError when fields does not match', () => {
+    const { sut } = makeSut('test', 'test2')
+    const error = sut.validate({ test: 'test', test2: 'invalid' })
+    expect(error).toEqual(new InvalidParamError('test2'))
+  })
+})
