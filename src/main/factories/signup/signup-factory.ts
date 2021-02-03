4728040939995ed95a8464562c55adf84035b022
Diego Pereira Lopes <diegoplopes5@gmail.com>
2021-02-03 17:47:59 -0300

    feat: add authentication to login factory

--- src/main/factories/signup/signup-factory.ts
+++ src/main/factories/signup/signup-factory.ts
@@ -1,9 +1,12 @@
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
 import { BcryptAdapter } from '../../../infra/cripto/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../infra/cripto/jwt-adapter/jwt-adapter'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
 import { SignUpController } from '../../../presentation/controllers/signup/signup-controller'
 import { Controller } from '../../../presentation/protocols'
+import env from '../../config/env'
 import { LogControllerDecorator } from '../../decorators/log'
 import { makeSignUpValidator } from './validators/signup-validators-factory'
 
@@ -12,7 +15,9 @@ export const makeSignUpController = (): Controller => {
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const addAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  const signUpController = new SignUpController(addAccount, makeSignUpValidator())
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const auth = new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
+  const signUpController = new SignUpController(addAccount, makeSignUpValidator(), auth)
   const logErrorRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpController, logErrorRepository)
 }
